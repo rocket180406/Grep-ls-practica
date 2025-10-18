5fb70f25d2b43243fd1c91ff4da32b6dfe1b9dfb
Daniel Herrero <rocketdans18@gmail.com>
2025-10-18 19:08:10 +0200

    programa subido
    commit 1

---
+++ TrabajoPSP/proceso/src/main/java/com/practica/Lanzador.java
@@ -0,0 +1,51 @@
+package com.practica;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Lanzador {
+
+    public static final String MSG_ERROR = "Se ha producido un error al ejecutar el comando";
+    public static final String[] COMANDO_LS = {"ls"};
+    public static final String[] COMANDO_GREP = {"grep", "a"};
+
+    public static void main(String[] args) {
+        try {
+
+            Process procesoLs = Runtime.getRuntime().exec(COMANDO_LS);
+
+            Process procesoGrep = Runtime.getRuntime().exec(COMANDO_GREP);
+
+            try (var outputLs = procesoLs.getInputStream(); var inputGrep = procesoGrep.getOutputStream()) {
+
+                outputLs.transferTo(inputGrep);
+            }
+
+            StringBuilder output = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(procesoGrep.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    output.append(line).append("\n");
+                }
+            }
+
+            int exitLs = procesoLs.waitFor();
+            int exitGrep = procesoGrep.waitFor();
+
+            if (exitLs == 0 && exitGrep == 0) {
+                System.out.println("Success!");
+                System.out.println(output);
+                System.exit(0);
+            } else {
+                System.out.println(MSG_ERROR);
+                System.exit(1);
+            }
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println(MSG_ERROR);
+            System.exit(34);
+        }
+    }
+}
